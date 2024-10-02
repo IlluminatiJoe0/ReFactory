8b022a1787b046201e3fe320f8a34e3d453442f4
IlluminatiJoe <action@dr.com>
2024-10-02 23:19:34 +0200

    EnderIO alloys as GT materials

--- kubejs/startup_scripts/materials/elements/elements.js
+++ kubejs/startup_scripts/materials/elements/elements.js
@@ -3,4 +3,6 @@ GTCEuStartupEvents.registry('gtceu:element', event => {
     // ₂₃₄₅₆₇₈₉₀
     event.create('ambrosium', 121, 302, -1, null, 'A', false)
     event.create('zanite', 122, 308, -1, null, 'Zt', false) 
+    event.create('energetic_alloy', 100, 100, -1, null, 'Si(FeS₂)₅(CrAl₂O₃)Hg₃Au', false)
+    event.create('vibrant_alloy', 100, 100, -1, null, '(Si(FeS₂)₅(CrAl₂O₃)Hg₃Au)(BeK₄N₅)', false)
  })
--- kubejs/startup_scripts/materials/materials.js
+++ kubejs/startup_scripts/materials/materials.js
@@ -1,5 +1,6 @@
 GTCEuStartupEvents.registry('gtceu:material', event => {
-
+    
+    // Ambrosium and Zanite from Aether
     event.create('ambrosium')
     .gem()
     .ore()
@@ -15,4 +16,53 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
     .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_LENS)
     .color(0x611791)
     .iconSet(GTMaterialIconSet.DIAMOND)
+
+    // Energetic and Vibrant Alloy from EnderIO
+    event.create('energetic_alloy')
+    .ingot()
+    .liquid()
+    .components(
+        '1x gtceu:gold_dust',
+        '1x minecraft:redstone'
+    )
+    .iconSet(GTMaterialIconSet.DULL)
+    .flags(
+        GTMaterialFlags.GENERATE_PLATE, 
+        GTMaterialFlags.GENERATE_LONG_ROD,
+        GTMaterialFlags.GENERATE_BOLT_SCREW,
+        GTMaterialFlags.GENERATE_DENSE,
+        GTMaterialFlags.GENERATE_RING,
+        GTMaterialFlags.GENERATE_FINE_WIRE,
+        GTMaterialFlags.GENERATE_FOIL,
+        GTMaterialFlags.GENERATE_GEAR,
+        GTMaterialFlags.GENERATE_ROD,
+        GTMaterialFlags.GENERATE_ROTOR
+    )
+    .element(GTElements.get('energetic_alloy'))
+    .blastTemp(1500, null, GTValues.VA[GTValues.MV], 20*20)
+
+    // Vibrant
+    event.create('vibrant_alloy')
+    .ingot()
+    .liquid()
+    .components(
+        '1x gtceu:energetic_alloy_dust',
+        '1x gtceu:ender_pearl_dust'
+    )
+    .iconSet(GTMaterialIconSet.DULL)
+    .flags(
+        GTMaterialFlags.GENERATE_PLATE, 
+        GTMaterialFlags.GENERATE_LONG_ROD,
+        GTMaterialFlags.GENERATE_BOLT_SCREW,
+        GTMaterialFlags.GENERATE_DENSE,
+        GTMaterialFlags.GENERATE_RING,
+        GTMaterialFlags.GENERATE_FINE_WIRE,
+        GTMaterialFlags.GENERATE_FOIL,
+        GTMaterialFlags.GENERATE_GEAR,
+        GTMaterialFlags.GENERATE_ROD,
+        GTMaterialFlags.GENERATE_ROTOR
+    )
+    .element(GTElements.get('vibrant_alloy'))
+    .blastTemp(1600, null, GTValues.VA[GTValues.MV], 20*25)
+
 })
