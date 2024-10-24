55a6d4305255362eb78f93867ca003d28bf24d6f
IlluminatiJoe <action@dr.com>
2024-10-24 19:22:09 +0200

    recipe fixes

---
+++ kubejs/server_scripts/recipes/mobGrindingUtils.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.replaceInput({id: 'mob_grinding_utils:recipe_tank_sink'}, 'minecraft:ender_eye', 'gtceu:aluminium_normal_fluid_pipe')
+    event.replaceInput({id: 'mob_grinding_utils:recipe_tank_sink'}, 'minecraft:iron_bars', 'gtceu:manasteel_plate')
+})
