226bf0c4b6ab32108cb50a1ba36130aa33bede8b
IlluminatiJoe <action@dr.com>
2024-12-02 15:17:39 +0100

    Minor fixes

---
+++ kubejs/server_scripts/recipes/space.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+    event.recipes.gtceu.forming_press("t1_welded_plate")
+      .itemInputs(['4x gtceu:zanite_steel_plate', '4x gtceu:black_steel_plate', '4x gtceu:titanium_plate', '4x gtceu:manasteel_plate'])
+      .circuit(1)
+      .itemOutputs('kubejs:t1_welded_rocket_plate')
+      .duration(20*8)
+      .EUt(GTValues.VA[GTValues.HV])
+
+    event.recipes.gtceu.implosion_compressor("t1_rocket_plate")
+      .itemInputs('kubejs:t1_welded_rocket_plate')
+      .circuit(1)
+      .itemOutputs('kubejs:t1_rocket_plate')
+      .duration(20)
+      .EUt(GTValues.VA[GTValues.HV])
+})
